f3f226ea3cb871a5e1976a054f1c54ae7b6d56e7
Humberto Dias <hldtux@gmail.com>
2020-08-31 00:22:04 +0200

    Update Log.cpp

--- src/Log.cpp
+++ src/Log.cpp
@@ -1,22 +1,25 @@
 #ifdef _WIN32
 #include <windows.h>
 #endif
-
 #include <stdio.h>
 #include "Globals.h"
 
 void
-log (const char *file, int line, const char *format, ...)
+log (const char file[], int line, const char *format, ...)
 {
   static char tmp_string[4096];
   static char tmp_string2[4096];
   static va_list ap;
-  #ifdef _WIN32
-  // Construct the string from variable arguments
+#ifdef _WIN32
   va_start (ap, format);
-  vsprintf_s(tmp_string, 4096, format, ap);
-  va_end(ap);
-  sprintf_s(tmp_string2, 4096, "\n%s(%d) : %s", file, line, tmp_string);
-  OutputDebugString(tmp_string2);
-  #endif
+  vsprintf_s (tmp_string, 4096, format, ap);
+  va_end (ap);
+  sprintf_s (tmp_string2, 4096, "\n%s(%d) : %s", file, line, tmp_string);
+  OutputDebugString (tmp_string2);
+#else 
+  va_start( ap, format );
+  vprintf( format, ap );
+  va_end( ap );
+  printf ("\n%s(%d)", file,line);
+#endif
 }
